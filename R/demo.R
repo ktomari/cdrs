25d156b1430fe4e03688367f0b07236556064420
ktomari <ktomari@ucdavis.edu>
2024-02-02 11:07:11 -0800

    updated example desc

--- R/demo.R
+++ R/demo.R
@@ -1,6 +1,8 @@
 #' Load fabricated example data.
 #'
-#' Using cdrs::cdrs_read(), loads demo data (inside extdata/demo)
+#' Using cdrs::cdrs_read(), loads demo data (inside extdata/demo). Do not draw
+#' conclusions from this fabricated "data set". The data were created by
+#' scrambling the public version of the DRS data.
 #'
 #' @param convert_to_NA logical. Whether to convert to NA or not.
 #' @return Returns a tibble of the a fictitious data set.
