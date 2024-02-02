407a7abf30c150df9bbb16bf50d58db2eb8a59b1
ktomari <ktomari@ucdavis.edu>
2024-02-02 14:24:02 -0800

    Added zipped version of example data

--- data-raw/demo_csv_data.R
+++ data-raw/demo_csv_data.R
@@ -151,33 +151,54 @@ if (!dir.exists(extdata_path)) {
   dir.create(extdata_path, recursive = TRUE)
 }
 
-# Write Csv ----
+# Write to File ----
 # Let's give it same date as our zip file.
 date_ <- basename(zip_) %>%
   str_extract("\\d{4}\\_\\d{2}\\_\\d{2}")
 
-write_csv(
-  x = dat2,
-  file = file.path(extdata_path, str_glue("DRS_demo_{date_}.csv"))
-)
-
-# Write Hash ----
-write(
-  x = digest(
-    object = file.path(extdata_path, str_glue("DRS_demo_{date_}.csv")),
-    algo = "sha256",
-    file = T
-  ),
-  file = file.path(extdata_path, str_glue("DRS_demo_{date_}.hash.txt"))
-)
-
-# Write Data Dictionary ----
-file.copy(
-  from = dd,
-  to = file.path(
+output_paths <- list(
+  csv = file.path(
+    extdata_path,
+    str_glue("DRS_demo_{date_}.csv")),
+  hash = file.path(
+    extdata_path,
+    str_glue("DRS_demo_{date_}.hash.txt")),
+  xlsx = file.path(
     extdata_path,
     str_glue("DRS_data_dictionary_demo_{date_}.xlsx")
   )
+)
+
+## Write to File ----
+write_csv(
+  x = dat2,
+  file = output_paths$csv
+)
+
+## Write Hash ----
+write(
+  x = digest(
+    object = output_paths$csv,
+    algo = "sha256",
+    file = T
+  ),
+  file = output_paths$hash
+)
+
+## Write Data Dictionary ----
+file.copy(
+  from = dd,
+  to = output_paths$xlsx
+)
+
+## Write to Zip ----
+
+zip(
+  zipfile = file.path(cwd,
+                      "inst",
+                      "extdata",
+                      str_glue("DRS_demo_{date_}.zip")),
+  files = output_paths %>% unlist()
 )
 
 unlink(temp_)
